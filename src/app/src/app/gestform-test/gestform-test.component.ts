46ac7781e6d4129e3dd5da3e8b49f8a6c7d470bc
jmoussalli <jerome.moussalli@gmail.com>
2024-04-10 10:48:54 +0200

    Génération d'un nombre aléatoire à la fois. La génération du nombre et l'application de la règle est un observable auquel ngOnInit() s'abonne pour mettre à jour le résultat affiché.

--- src/app/src/app/gestform-test/gestform-test.component.ts
+++ src/app/src/app/gestform-test/gestform-test.component.ts
@@ -1,50 +1,41 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {Observable, interval} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-gestform-test',
   standalone: true,
-  imports: [
-    NgForOf
-  ],
   templateUrl: './gestform-test.component.html',
   styleUrls: ['./gestform-test.component.css']
 })
 export class GestformTestComponent implements OnInit {
-  numbers: number[] = [];
-  results: string[] = [];
+  currentNumber?: number;
+  currentResult?: string;
 
   ngOnInit(): void {
-    this.generateNumbersContinuously();
+    this.generateNumbersContinuously().subscribe((result: string) => {
+      this.currentResult = result;
+    });
   }
 
-  generateNumbersContinuously(): void {
-    setInterval(() => {
-      if (this.numbers.length >= 10) { // Limite la liste des résultats affichée à 10 lignes
-        this.numbers.shift(); // Supprime le premier élément si la liste contient déjà 10 nombres
-        this.results.shift(); // Supprime également le premier résultat pour garder les listes synchronisées
-      }
-      const randomNumber = Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000);
-      this.numbers.push(randomNumber);
-      this.applyRule(randomNumber);
-    }, 1000); // Génère un nombre toutes les secondes
+  generateNumbersContinuously(): Observable<string> {
+    return interval(1000).pipe(
+      map(() => {
+        this.currentNumber = Math.floor(Math.random() * (2001)) - 1000;
+        return this.applyRule(this.currentNumber);
+      })
+    );
   }
 
-  applyRule(number: number): void {
-    let result;
+  applyRule(number: number): string {
     if (number % 3 === 0 && number % 5 === 0) {
-      result = number.toString() + ' : Gestform';
+      return `${number} : Gestform`;
     } else if (number % 3 === 0) {
-      result = number.toString() + ' : Geste';
+      return `${number} : Geste`;
     } else if (number % 5 === 0) {
-      result = number.toString() + ' : Forme';
+      return `${number} : Forme`;
     } else {
-      result = number.toString();
+      return number.toString();
     }
-    this.results.push(result);
-    console.log(number);
-    console.log(this.results);
-    console.log(this.numbers.length);
-    console.log(this.results.length);
   }
 }
