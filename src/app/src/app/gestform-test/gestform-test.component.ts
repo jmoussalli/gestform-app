a01c26da5c3a168b4e345960318a1c7b1d616a6a
jmoussalli <jerome.moussalli@gmail.com>
2024-04-10 10:25:03 +0200

    Algorithme d'application des opérations. Liste de nombres infinie en entrée. Affichage des 10 derniers résultats en continu.

--- src/app/src/app/gestform-test/gestform-test.component.ts
+++ src/app/src/app/gestform-test/gestform-test.component.ts
@@ -15,30 +15,36 @@ export class GestformTestComponent implements OnInit {
   results: string[] = [];
 
   ngOnInit(): void {
-    this.generateRandomNumbers();
-    this.applyRules();
+    this.generateNumbersContinuously();
   }
 
-  generateRandomNumbers(): void {
-    for (let i = 0; i < 10; i++) { // Générer 10 nombres pour l'exemple
+  generateNumbersContinuously(): void {
+    setInterval(() => {
+      if (this.numbers.length >= 10) { // Limite la liste des résultats affichée à 10 lignes
+        this.numbers.shift(); // Supprime le premier élément si la liste contient déjà 10 nombres
+        this.results.shift(); // Supprime également le premier résultat pour garder les listes synchronisées
+      }
       const randomNumber = Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000);
       this.numbers.push(randomNumber);
-    }
+      this.applyRule(randomNumber);
+    }, 1000); // Génère un nombre toutes les secondes
   }
 
-  applyRules(): void {
-    this.numbers.forEach(number => {
-      if (number % 3 === 0 && number % 5 === 0) {
-        this.results.push('Gestform');
-      } else if (number % 3 === 0) {
-        this.results.push('Geste');
-      } else if (number % 5 === 0) {
-        this.results.push('Forme');
-      } else {
-        this.results.push(number.toString());
-      }
-      console.log(number);
-      console.log(this.results);
-    });
+  applyRule(number: number): void {
+    let result;
+    if (number % 3 === 0 && number % 5 === 0) {
+      result = number.toString() + ' : Gestform';
+    } else if (number % 3 === 0) {
+      result = number.toString() + ' : Geste';
+    } else if (number % 5 === 0) {
+      result = number.toString() + ' : Forme';
+    } else {
+      result = number.toString();
+    }
+    this.results.push(result);
+    console.log(number);
+    console.log(this.results);
+    console.log(this.numbers.length);
+    console.log(this.results.length);
   }
 }
